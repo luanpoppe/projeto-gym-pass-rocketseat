52876b41f72a177256dc3851f004c9d81992343f
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-24 13:36:03 -0300

    feat: adicionando hendler global de erros

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
 import fastify from "fastify";
-import { register } from "./http/controllers/register.controller";
 import { appRoutes } from "./http/routes";
+import { z, ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError)
+    return reply.status(400).send({
+      message: "Validation error.",
+      issues: z.treeifyError(error),
+    });
+
+  if (env.NODE_ENV !== "production") console.error(error);
+
+  return reply.status(500).send({ message: "Internal server error" });
+});
