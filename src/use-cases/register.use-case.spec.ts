b41a6bbeae4ded03f1ae34bf3563f99d46f4219d
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-24 14:17:46 -0300

    feat: adicionando InMemoryRepository e primeiros testes unitários do registro de usuários

---
+++ src/use-cases/register.use-case.spec.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it } from "vitest";
+import { RegisterUseCase } from "./register.user-case";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists.error";
+
+describe("Register Use Case", () => {
+  it("should be able to register", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "abcd",
+      email: "abcd@gmail.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should hash user password upon registration", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "abcd",
+      email: "abcd@gmail.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it.only("should not be able to register with same email twice", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const email = "abcd@gmail.com";
+
+    await registerUseCase.execute({
+      name: "abcd",
+      email,
+      password: "123456",
+    });
+
+    const result = registerUseCase.execute({
+      name: "abcd",
+      email,
+      password: "123456",
+    });
+
+    expect(result).rejects.toBeInstanceOf(UserAlreadyExistsError);
+  });
+});
