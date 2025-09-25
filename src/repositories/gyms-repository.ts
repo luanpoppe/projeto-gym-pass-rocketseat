b63a4cd2541d48d9efc52c1f41815292ef24b319
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-25 14:32:57 -0300

    feat: adicionando funcionalidade de buscar gyms por nome

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -3,5 +3,7 @@ import { Gym, Prisma } from "generated/prisma";
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>;
 
+  searchMany(query: string, page: number): Promise<Gym[]>;
+
   create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
---
+++ src/use-cases/search-gyms.use-case.spec.ts
@@ -0,0 +1,72 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { FetchUserCheckInsHistoryUseCase } from "./fetch-user-check-ins-history.use-case";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { SearchGymsUseCase } from "./search-gyms.use-case";
+
+let gymsRepository: InMemoryGymsRepository;
+let searchGymsUseCase: SearchGymsUseCase;
+
+describe("Search Gyms Use Case", () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository();
+    searchGymsUseCase = new SearchGymsUseCase(gymsRepository);
+  });
+
+  it("should be able to search for gyms", async () => {
+    await gymsRepository.create({
+      title: "Academia abc",
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    });
+
+    await gymsRepository.create({
+      title: "Academia def",
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    });
+
+    const { gyms } = await searchGymsUseCase.execute({
+      query: "abc",
+      page: 1,
+    });
+
+    expect(gyms).toHaveLength(1);
+    expect(gyms).toEqual([
+      expect.objectContaining({
+        title: "Academia abc",
+      }),
+    ]);
+  });
+
+  it("should be able to fetch paginated gyms search", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `Academia ${i}`,
+        description: null,
+        phone: null,
+        latitude: 0,
+        longitude: 0,
+      });
+    }
+
+    const { gyms } = await searchGymsUseCase.execute({
+      query: "Academia",
+      page: 2,
+    });
+
+    expect(gyms).toHaveLength(2);
+    expect(gyms).toEqual([
+      expect.objectContaining({
+        title: "Academia 21",
+      }),
+      expect.objectContaining({
+        title: "Academia 22",
+      }),
+    ]);
+  });
+});
