3e7718b7dae27dfab638ec8c862f5c51809d27ef
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-26 14:49:45 -0300

    feat: adicionando controllers de gyms

---
+++ src/http/controllers/gyms/search.controller.ts
@@ -0,0 +1,21 @@
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { page, q } = searchGymsQuerySchema.parse(request.body);
+
+  const searchGymUseCase = makeSearchGymsUseCase();
+
+  const { gyms } = await searchGymUseCase.execute({
+    page,
+    query: q,
+  });
+
+  return reply.status(200).send({ gyms });
+}
